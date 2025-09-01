96c1e886687fecdcbeabddc2f779fe48f52272f5
DaNiLo <danilo.p2391@gmail.com>
2025-09-01 14:17:57 +0200

    Properly handle all error cases of BadRequests on get_posts POST

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -2,36 +2,51 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
+app.json.sort_keys = False
 CORS(app)  # This will enable CORS for all routes
 
 POSTS = []
 
 
 @app.errorhandler(400)
-def missing_fields(_e):
-    """Doesn't allow missing fields."""
-    return jsonify({"Bad Request: Missing fields"}), 400
+def bad_requests(error):
+    """Return a custom 400 error."""
+    return jsonify(error), 400
 
 
 @app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    """Shows all posts and can add a new one."""
+    """GET returns all posts, POST add a new one."""
     if request.method == 'POST':
         body = request.get_json()
 
-        # Don't allow missing required fields
-        if not body["title"] or not body["content"]:
-            return 'Missing fields!', 400
+        # Check for missing fields
+        try:
+            # Check for empty values
+            for field, value in body.items():
+                if value.strip() == "":
+                    raise ValueError(f"Required '{field}' is empty.")
 
-        new_posts = {
-            "id": POSTS[-1]["id"] + 1 if len(POSTS) > 0 else 1,
-            "title": body["title"],
-            "content": body["content"]
-        }
-        # Adds new post to the posts list
-        POSTS.append(new_posts)
+            # New Post ready to add with AutoIncrementing ID
+            new_posts = {
+                "id": POSTS[-1]["id"] + 1 if len(POSTS) > 0 else 1,
+                "title": str(body["title"]),
+                "content": str(body["content"])
+            }
+
+            # Adds new post to the posts list
+            POSTS.append(new_posts)
+
+        # Handle missing fields or empty values
+        except KeyError as key_error:
+            return f'Missing field: {key_error}', 400
+
+        except ValueError as value_error:
+            return f'Invalid field: {value_error}', 400
+
         return jsonify(new_posts), 201
-    # GET request returns a list of posts
+
+    # GET request returns the posts list
     return jsonify(POSTS)
 
 
