7949c9a83845b9ebaa38fe325cb08bd4a0e25e80
DaNiLo <danilo.p2391@gmail.com>
2025-09-01 13:17:37 +0200

    Add logic to add new post and handle errors

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,17 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)  # This will enable CORS for all routes
 
-POSTS = [
-    {"id": 1, "title": "First post", "content": "This is the first post."},
-    {"id": 2, "title": "Second post", "content": "This is the second post."},
-]
+POSTS = []
 
 
-@app.route('/api/posts', methods=['GET'])
+@app.errorhandler(400)
+def missing_fields(_e):
+    """Doesn't allow missing fields."""
+    return jsonify({"Bad Request: Missing fields"}), 400
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    """Shows all posts and can add a new one."""
+    if request.method == 'POST':
+        body = request.get_json()
+
+        # Don't allow missing required fields
+        if not body["title"] or not body["content"]:
+            return 'Missing fields!', 400
+
+        new_posts = {
+            "id": POSTS[-1]["id"] + 1 if len(POSTS) > 0 else 1,
+            "title": body["title"],
+            "content": body["content"]
+        }
+        # Adds new post to the posts list
+        POSTS.append(new_posts)
+        return jsonify(new_posts), 201
+    # GET request returns a list of posts
     return jsonify(POSTS)
 
 
